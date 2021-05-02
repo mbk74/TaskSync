5da8cba66ba8afe430e9b050c93b97a601cafef9
Max Kolesnik <maximk@marka-software.com>
2021-05-02 22:49:15 +0300

    Added project

---
+++ TaskSync/TaskSync/Provider.h
@@ -0,0 +1,17 @@
+//
+//  Provider.h
+//  TaskSync
+//
+//  Created by Max Kolesnik on 02.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Provider : NSObject
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TaskSync/TaskSync/Provider.m
@@ -0,0 +1,25 @@
+//
+//  Provider.m
+//  TaskSync
+//
+//  Created by Max Kolesnik on 02.05.2021.
+//
+
+#import "Provider.h"
+@interface Provider ()
+
+@property (nonatomic, strong) NSObject* syncronizer;
+
+@end
+
+@implementation Provider
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        <#statements#>
+    }
+    return self;
+}
+@end
